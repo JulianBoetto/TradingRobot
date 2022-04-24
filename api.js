198d19ffc36f52b25a3f44a8ff3cad16723306bd
Julian Boetto <julib_8724@hotmail.com>
2022-04-23 22:02:05 -0300

    Recebendo account info da conta teste

--- api.js
+++ api.js
@@ -1,12 +1,42 @@
 const axios = require('axios');
-const querystring = require('querystring')
+const crypto = require('crypto')
+
+const apiUrl = process.env.API_URL
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY;
+
+async function privateCall(path, data = {}, method = 'GET') {
+    const timestamp = Date.now();
+    const signature = crypto.createHmac('sha256', apiSecret)
+                    .update(`${new URLSearchParams({...data,timestamp}).toString()}`)
+                    .digest('hex');
+    
+    const newData = { ...data, timestamp, signature };
+    const qs = `?${new URLSearchParams(newData).toString()}`
+
+    try {
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey }
+        })
+
+        return result.data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function accountInfo() {
+    return privateCall('/v3/account')
+}
 
 async function publicCall(path, data, method = 'GET') {
     try {
         const qs = data ? `?${new URLSearchParams(data).toString()}` :  '';
         const result = await axios({
             method,
-            url: `${process.env.API_URL}${path}${qs}`
+            url: `${apiUrl}${path}${qs}`
         })
         return result.data
     } catch (error) {
@@ -26,4 +56,4 @@ async function exchangeInfo() {
     return publicCall('/v3/exchangeInfo')
 }
 
-module.exports = { time, depth, exchangeInfo }
+module.exports = { time, depth, exchangeInfo, accountInfo }
--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const api = require('./api');
 const symbol = process.env.SYMBOL
 
 setInterval(async () => {
-    const result = await api.depth();
-    console.log(`Maior preço de compra: ${result.bids[0][0]}`);
-    console.log(`Menor preço de venda: ${result.asks[0][0]}`);
+    let buy = 0, sell = 0;
+    const result = await api.depth(symbol);
+    if(result.bids && result.bids.length ){
+        console.log(`Maior preço de compra: ${result.bids[0][0]}`);
+        buy = parseInt(result.bids[0][0]);
+    }
 
-    const sell = parseInt(result.asks[0][0]);
-    const buy = parseInt(result.bids[0][0]);
+    if(result.asks && result.asks.length){
+        console.log(`Menor preço de venda: ${result.asks[0][0]}`);
+        sell = parseInt(result.asks[0][0]);
+    }
+
     if(sell < 192300 ) {
         console.log('Hora de comprar!!')
+        console.log(await api.accountInfo());
     }
 
     else if(buy > 200000 ) {
