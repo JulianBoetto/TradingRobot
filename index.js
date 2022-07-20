0555f1adbfa81ed9a76a6da4fbb23db40a0ea06d
Julián Boetto <julib_8724@hotmail.com>
2022-07-20 17:08:28 +0200

    Express instalado

--- index.js
+++ index.js
@@ -1,56 +1,68 @@
 const api = require('./api');
+const path = require("path")
+const express = require("express")
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+const PORT = 3000;
 const symbol = process.env.SYMBOL;
 const profitability = parseFloat(process.env.PROFITABILITY);
 const coin = process.env.COIN;
 const goodBuy = process.env.GOOD_BUY;
 
-setInterval(async () => {
-    let buy = 0, sell = 0;
-    const result = await api.depth(symbol);
-    if(result.bids && result.bids.length ){
-        console.log(`Maior preço de compra: ${result.bids[0][0]}`);
-        buy = parseFloat(result.bids[0][0]);
-    }
+app.get('/', function(req, res) {
+    res.sendFile('views/index.html', {root: __dirname })
+});
 
-    if(result.asks && result.asks.length){
-        console.log(`Menor preço de venda: ${result.asks[0][0]}`);
-        sell = parseFloat(result.asks[0][0]);
-    }
+app.listen(PORT, () => console.log(`Server run in port: ${PORT}`))
 
-    if(sell && sell < goodBuy ) {
-        console.log('Hora de comprar!!')
+// setInterval(async () => {
+//     let buy = 0, sell = 0;
+//     const result = await api.depth(symbol);
+//     if(result.bids && result.bids.length ){
+//         console.log(`Maior preço de compra: ${result.bids[0][0]}`);
+//         buy = parseFloat(result.bids[0][0]);
+//     }
 
-        const account = await api.accountInfo();
-        const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1);
-        console.log(`POSIÇÃO DA CARTEIRA`)
-        console.log(coins);
+//     if(result.asks && result.asks.length){
+//         console.log(`Menor preço de venda: ${result.asks[0][0]}`);
+//         sell = parseFloat(result.asks[0][0]);
+//     }
+
+//     if(sell && sell < goodBuy ) {
+//         console.log('Hora de comprar!!')
+
+//         const account = await api.accountInfo();
+//         const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1);
+//         console.log(`POSIÇÃO DA CARTEIRA`)
+//         console.log(coins);
         
-        console.log('Verificando o meu dinheiro...')
-        const walletCoin = parseFloat(coins.find(c => c.asset === coin).free)
-        console.log(walletCoin)
-        if(sell <= walletCoin){
-            console.log('Temos dinheiro, comprando agora...')
-            const buyOrder = await api.newOrder(symbol, 1)
-            console.log(`orderId: ${buyOrder.orderId}`);
-            console.log(`status: ${buyOrder.status}`);
+//         console.log('Verificando o meu dinheiro...')
+//         const walletCoin = parseFloat(coins.find(c => c.asset === coin).free)
+//         console.log(walletCoin)
+//         if(sell <= walletCoin){
+//             console.log('Temos dinheiro, comprando agora...')
+//             const buyOrder = await api.newOrder(symbol, 1)
+//             console.log(`orderId: ${buyOrder.orderId}`);
+//             console.log(`status: ${buyOrder.status}`);
 
-            if(buyOrder.status === 'FILLED') {
-                console.log('Posicionando venda futura');
-                const price = parseFloat(sell * profitability).toFixed(5);
-                console.log(`Vendendo por ${price} (${profitability})`)
-                const sellOrder = await api.newOrder(symbol, 1, price, 'SELL', 'LIMIT');
-                console.log(`orderId: ${sellOrder.orderId}`);
-                console.log(`status: ${sellOrder.status}`);
-            }
+//             if(buyOrder.status === 'FILLED') {
+//                 console.log('Posicionando venda futura');
+//                 const price = parseFloat(sell * profitability).toFixed(5);
+//                 console.log(`Vendendo por ${price} (${profitability})`)
+//                 const sellOrder = await api.newOrder(symbol, 1, price, 'SELL', 'LIMIT');
+//                 console.log(`orderId: ${sellOrder.orderId}`);
+//                 console.log(`status: ${sellOrder.status}`);
+//             }
 
-        }
-    }
+//         }
+//     }
 
-    else if(buy && buy > goodBuy ) {
-        console.log('Hora de vender!!')
-    } 
+//     else if(buy && buy > goodBuy ) {
+//         console.log('Hora de vender!!')
+//     } 
 
-    else {
-        console.log('Esperando...')
-    }
-}, process.env.CRAWLER_INTERVAL);
+//     else {
+//         console.log('Esperando...')
+//     }
+// }, process.env.CRAWLER_INTERVAL);
