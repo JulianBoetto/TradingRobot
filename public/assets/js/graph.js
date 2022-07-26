0daf918c9946936886a3887ad117d3978cf3c78f
Julián Boetto <julib_8724@hotmail.com>
2022-07-26 17:50:01 +0200

    Mostrando o grafico novo com pusher

--- public/assets/js/graph.js
+++ public/assets/js/graph.js
@@ -1,5 +1,4 @@
 const socket = io();
-const Pusher = require('pusher');
 
 const form = document.getElementById('searchForm');
 const input = document.getElementById('input');
@@ -10,6 +9,7 @@ const coinNameElement = document.getElementById('coinName');
 // console.log(atualValue)
 
 
+// const Pusher = require('pusher');
 
 let coinName = "btcusdt";
 
@@ -31,53 +31,94 @@ socket.on('coin', function(msg) {
     }
 }); 
 
+var londonTempData = {
+    city: 'London',
+    unit: 'celsius',
+    dataPoints: [
+      {
+        time: 1130,
+        temperature: 12 
+      },
+      {
+        time: 1200,
+        temperature: 13 
+      },
+      {
+        time: 1230,
+        temperature: 15 
+      },
+      {
+        time: 1300,
+        temperature: 14 
+      },
+      {
+        time: 1330,
+        temperature: 15 
+      },
+      {
+        time: 1406,
+        temperature: 12 
+      },
+    ]
+  }
 
-document.addEventListener("DOMContentLoaded", () => {
-    new ApexCharts(document.querySelector("#reportsChart"), {
-      series: [{
-        name: 'Sales',
-        data: [31, 40, 28, 51, 42, 82, 56],
-      }],
-      chart: {
-        height: 350,
-        type: 'area',
-        toolbar: {
-          show: false
-        },
-      },
-      markers: {
-        size: 4
-      },
-      colors: ['#4154f1', '#2eca6a', '#ff771d'],
-      fill: {
-        type: "gradient",
-        gradient: {
-          shadeIntensity: 1,
-          opacityFrom: 0.3,
-          opacityTo: 0.4,
-          stops: [0, 90, 100]
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: 'smooth',
-        width: 2
-      },
-      xaxis: {
-        type: 'datetime',
-        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-      },
-      tooltip: {
-        x: {
-          format: 'dd/MM/yy HH:mm'
-        },
-      }
-    }).render();
-  });
+function showEle(elementId){
+  document.getElementById(elementId).style.display = 'flex';
+}
 
+function hideEle(elementId){
+  document.getElementById(elementId).style.display = 'none';
+}
 
-// form.addEventListener("submit", (event) => {
-//     getCoin(event)
-// });
+function renderWeatherChart(weatherData) {
+    var ctx = document.getElementById("weatherChart").getContext("2d");
+    var options = { };
+    weatherChartRef = new Chart(ctx, {
+      type: "line",
+      data: weatherData,
+      options: options
+    });
+ }
+
+ var chartConfig = {
+    labels: [],
+    datasets: [
+       {
+          label: "London Weather",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [],
+          spanGaps: false,
+       }
+    ]
+ };
+
+ function onFetchTempSuccess(){
+    hideEle("loader");
+    // var londonTempData = londonTempData;
+    console.log(londonTempData)
+    chartConfig.labels = londonTempData.dataPoints.map(dataPoint => dataPoint.time);
+    chartConfig.datasets[0].data = londonTempData.dataPoints.map(dataPoint => dataPoint.temperature);
+    renderWeatherChart(chartConfig)
+  }
+  
+  
+(
+  showEle("loader"),
+  onFetchTempSuccess
+())
