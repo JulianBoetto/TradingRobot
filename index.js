0e562510896f602ac69c7c5d10e3523e0fb8b88c
Julián Boetto <julib_8724@hotmail.com>
2022-07-26 20:27:21 +0200

    Sem socket

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const api = require('./api');
 const path = require("path");
-const ejs = require("ejs")
-const WebSocket = require("ws")
 const express = require("express");
 const { json } = require('express');
 const app = express(json);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 const PORT = 3000;
 const symbol = process.env.SYMBOL;
 const profitability = parseFloat(process.env.PROFITABILITY);
 const coin = process.env.COIN;
 const goodBuy = process.env.GOOD_BUY;
-const moment = require("moment");
 
-// const coinName = document.getElementById('coinName');
-
-
-let cryptoData = "";
 
 app.get('/', (req, res) => {
     res.sendFile('views/index.html', { root: __dirname, teste: "teste" })
@@ -38,10 +27,6 @@ app.get('/contact', (req, res) => {
 
 app.get('/trading', async (req, res) => {
     try {
-        // const result = await api.depth(symbol);
-
-        getCriptoValue()
-
         res.sendFile('views/trading.html', { root: __dirname })
     } catch (error) {
         console.log(error)
@@ -50,40 +35,4 @@ app.get('/trading', async (req, res) => {
 });
 
 
-server.listen(process.env.PORT || PORT, () => console.log(`Server run in port: ${process.env.PORT || PORT}`))
-
-let connections = false;
-
-async function getCriptoValue() {
-    let coinName;
-
-    io.on("connection", (socket) => {
-        console.log("cliente conectado", socket.id)
-
-        coinName = "btcusdt"
-        const ws = new WebSocket(`wss://stream.binance.com:9443/ws/ticker`);
-
-        ws.onopen = () => {
-            connections = true
-            ws.send(JSON.stringify({
-                "method": "SUBSCRIBE",
-                "params": [
-                    `${coinName}@ticker`
-                ],
-                "id": 1
-            }));
-        }
-
-        let num = 0;
-
-        ws.onmessage = (event) => {
-            process.stdout.write("\033c")
-            data = JSON.parse(event.data)
-            num += 1
-            data.time = num
-            io.emit('coin', data);
-        }
-    })
-}
-
-const teste = getCriptoValue()
+app.listen(process.env.PORT || PORT, () => console.log(`Server run in port: ${process.env.PORT || PORT}`))
