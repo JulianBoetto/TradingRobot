b12ab07ea60e26603221f642a1f74b9b6d6b5d36
Julián Boetto <julib_8724@hotmail.com>
2022-09-23 20:35:21 +0200

    Symbols crud ok

--- src/controllers/symbolsController.js
+++ src/controllers/symbolsController.js
@@ -1,10 +1,35 @@
 const api = require('../api');
+const Symbols = require("../models/symbols");
 
 class SymbolsController {
-    async allOrders(req, res) {
+    async allSymbols(req, res) {
         try {
-            
-            res.status(200).send(orders);
+            const symbols = await Symbols.find();
+            res.status(200).send(symbols);
+        } catch (error) {
+            res.status(401).send(error);
+        }
+    }
+
+    async createSymbols(req, res) {
+        try {
+            const { symbol, pair } = req.body;
+            symbol.toUpperCase();
+            pair.toUpperCase();
+            const symbols = await Symbols.create({ symbol, pair });
+            res.status(200).send(symbols);
+        } catch (error) {
+            res.status(401).send(error);
+        }
+    }
+
+    async removeSymbols(req, res) {
+        try {
+            const { symbol, pair } = req.body;
+            symbol.toUpperCase();
+            pair.toUpperCase();
+            const symbols = await Symbols.remove({ symbol: symbol, pair: pair });
+            res.status(200).send(symbols);
         } catch (error) {
             res.status(401).send(error);
         }
---
+++ src/routes/symbols.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const api = require('../api');
+const { verifyToken } = require("../lib/password");
+const SymbolsController = require("../controllers/symbolsController");
+const symbolsController = new SymbolsController();
+
+router.post('/allSymbols', verifyToken, symbolsController.allSymbols);
+router.post('/createSymbol', verifyToken, symbolsController.createSymbols);
+router.delete('/removeSymbol', verifyToken, symbolsController.removeSymbols);
+
+module.exports = router;
